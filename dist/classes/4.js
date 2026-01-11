34f8bb9d2a8072a069fd9f2dd8e0e1ac812c895b
basesnel <basesnel@gmail.com>
2026-01-11 13:33:54 +0200

    Comment for task 4 is uploaded.

--- dist/classes/4.js
+++ dist/classes/4.js
@@ -21,6 +21,15 @@
 в якому людина приходить додому.
 
 Наприклад, ось так:
+
+  const key = new Key();
+
+  const house = new MyHouse(key);
+  const person = new Person(key);
+
+  house.openDoor(person.getKey());
+
+  house.comeIn(person);
 */
 class Key {
     constructor() {
