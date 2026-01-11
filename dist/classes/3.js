910bdf67ca10bbb85312df4ad6320f74188fefaf
basesnel <basesnel@gmail.com>
2026-01-11 13:38:21 +0200

    Comment for task 3 is uploaded.

--- dist/classes/3.js
+++ dist/classes/3.js
@@ -5,8 +5,36 @@
   Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard.
   Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp.
   Інтерфейс ISpellCaster повинен включати метод castSpell.
+
+  // реалізація класу Wizard
+  class Wizard implements ICharacter, ISpellCaster {
+    constructor(public name: string, public level: number) {
+      if (this.level < 1) {
+        throw new Error('Level too low');
+      }
+    }
+
+    introduce(phrase: string): void {
+      console.log(phrase + ', ' + this.name);
+    }
+
+    castSpell(): void {
+      console.log('Casting a spell, behold my power!');
+    }
+
+    levelUp(): void {
+      this.level++;
+      console.log(`Level up! New level is ${this.level}`);
+    }
+  }
+
+  // тестування класу
+  const wizard = new Wizard('Merlin', 15);
+
+  wizard.introduce('I am the mighty wizard');
+  wizard.castSpell();
+  wizard.levelUp();  // Level up! New level is 16
 */
-// реалізація класу Wizard
 class Wizard {
     constructor(name, level) {
         this.name = name;
@@ -26,10 +54,5 @@ class Wizard {
         console.log(`Level up! New level is ${this.level}`);
     }
 }
-// тестування класу
-// const wizard = new Wizard('Merlin', 15);
-// wizard.introduce('I am the mighty wizard');
-// wizard.castSpell();
-// wizard.levelUp(); // Level up! New level is 16
 export { Wizard };
 //# sourceMappingURL=3.js.map
--- src/classes/3.ts
+++ src/classes/3.ts
@@ -5,36 +5,36 @@
   Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard.
   Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp.
   Інтерфейс ISpellCaster повинен включати метод castSpell.
+
+  // реалізація класу Wizard
+  class Wizard implements ICharacter, ISpellCaster {
+    constructor(public name: string, public level: number) {
+      if (this.level < 1) {
+        throw new Error('Level too low');
+      }
+    }
+
+    introduce(phrase: string): void {
+      console.log(phrase + ', ' + this.name);
+    }
+
+    castSpell(): void {
+      console.log('Casting a spell, behold my power!');
+    }
+
+    levelUp(): void {
+      this.level++;
+      console.log(`Level up! New level is ${this.level}`);
+    }
+  }
+
+  // тестування класу
+  const wizard = new Wizard('Merlin', 15);
+
+  wizard.introduce('I am the mighty wizard');
+  wizard.castSpell();
+  wizard.levelUp();  // Level up! New level is 16
 */
-
-// // реалізація класу Wizard
-// class Wizard implements ICharacter, ISpellCaster {
-//   constructor(public name: string, public level: number) {
-//     if (this.level < 1) {
-//       throw new Error('Level too low');
-//     }
-//   }
-
-//   introduce(phrase: string): void {
-//     console.log(phrase + ', ' + this.name);
-//   }
-
-//   castSpell(): void {
-//     console.log('Casting a spell, behold my power!');
-//   }
-
-//   levelUp(): void {
-//     this.level++;
-//     console.log(`Level up! New level is ${this.level}`);
-//   }
-// }
-
-// // тестування класу
-// const wizard = new Wizard('Merlin', 15);
-
-// wizard.introduce('I am the mighty wizard');
-// wizard.castSpell();
-// wizard.levelUp(); // Level up! New level is 16
 
 interface ICharacter {
   name: string;
@@ -47,7 +47,6 @@ interface ISpellCaster {
   levelUp(): void;
 }
 
-// реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -68,12 +67,5 @@ class Wizard implements ICharacter, ISpellCaster {
     console.log(`Level up! New level is ${this.level}`);
   }
 }
-
-// тестування класу
-// const wizard = new Wizard('Merlin', 15);
-
-// wizard.introduce('I am the mighty wizard');
-// wizard.castSpell();
-// wizard.levelUp(); // Level up! New level is 16
 
 export { Wizard };
